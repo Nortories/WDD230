b79105b1539bee8c6cca38b4826bad31991c0681
Josh <sooaemalelagij@gmail.com>
2023-10-01 14:03:23 -0600

    most-upto date

--- lesson2/scripts/bom.js
+++ lesson2/scripts/bom.js
@@ -2,13 +2,29 @@ const input = document.getElementById('favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
+
+
+
 button.addEventListener('click', () => {
     if(input.value){
         //TEMP
         console.log('button clicked');
+        const deleteButton = document.createElement('button');
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', () => {
+        console.log('delete button clicked');
+        list.removeChild(li);
+        input.focus();
+        });
     }
     else{
         input.focus();
         alert('Please enter a chapter');
     }
 });
+
---
+++ lesson2/scripts/hamburger.js
@@ -0,0 +1,8 @@
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('_______', () => {
+	navigation._______.toggle('_______');
+	hamButton.classList.toggle('open');
+});
