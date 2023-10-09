aa6a92295f9673b62d15f0b957ee6a595904b4b6
Josh <sooaemalelagij@gmail.com>
2023-10-09 11:56:39 -0600

    Update openWeatherMap.js
    api call from http to https

--- scripts/openWeatherMap.js
+++ scripts/openWeatherMap.js
@@ -6,7 +6,7 @@ const weatherElement = document.getElementById('weather-container');
 
 // Get the weather data from the API
 async function getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&forecast?id=524901&appid=${apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&forecast?id=524901&appid=${apiKey}`);
     const data = await response.json();
 
     if (response.ok) {
