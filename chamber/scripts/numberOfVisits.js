4a57ccdd02064c6d44852e6bb9f2d2bc24384869
Josh <sooaemalelagij@gmail.com>
2023-10-25 00:25:28 -0600

    commit

--- chamber/scripts/numberOfVisits.js
+++ chamber/scripts/numberOfVisits.js
@@ -1,7 +1,24 @@
 let visitElement = document.getElementById('visits');
+let message = document.getElementById('visit-message');
+
+const currentDay = new Date().getDay();
 
 function displayNumberOfVisits() {
     visitElement.innerHTML = getNumberOfVisits();
+}
+function displayMessage() {
+    var lastVisit = localStorage.getItem('lastVisit');
+    if (!lastVisit) {
+        lastVisit = currentDay;
+        setNewDate();
+        message.html = "Welcome! Let us know if you have any questions.";
+    }
+    else if (lastVisit == currentDay) {
+        message.innerHTML = "Back so soon! Awesome!";
+    }
+    else {
+        message.innerHTML = `You last visited ${lastVisit} days ago.`;
+    }
 }
 
 function getNumberOfVisits() {
@@ -14,4 +31,12 @@ function getNumberOfVisits() {
     return numberOfVisits;
 }
 
+function setNewDate() {
+    
+    localStorage.setItem('lastVisit', currentDay);
+}
+
+
+setNewDate();
 displayNumberOfVisits();
+displayMessage();
