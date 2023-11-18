2a83bf4dd988324082a89a8077d1a2e8f6d99099
nortories <sooaemalelagij@gmail.com>
2023-11-18 14:37:59 -0700

    Update links.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,31 +1,34 @@
 const baseURL = "https://nortories.github.io/wdd230/";
-const linksURL = "https://nortories.github.io/wdd230/data/links.json";
+const linksURL = "./data/links.json";
 
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    console.log("script running");
+    console.log(data);
+    displayLinks(data.lessons);
   }
-function displayLinks(weeks) {
+  
+function displayLinks(lessons) {
     const linksList = document.getElementById("linksList");
-    weeks.forEach(week => {
-        const weekItem = document.createElement("li");
-        const weekTitle = document.createElement("h3");
-        weekTitle.textContent = `Week ${week.week}`;
-        weekItem.appendChild(weekTitle);
-        const links = week.links;
-        const linksList = document.createElement("ul");
-        links.forEach(link => {
-            const linkItem = document.createElement("li");
-            const linkAnchor = document.createElement("a");
-            linkAnchor.textContent = link.label;
-            linkAnchor.setAttribute("href", link.url);
-            linkItem.appendChild(linkAnchor);
-            linksList.appendChild(linkItem);
-        });
-        weekItem.appendChild(linksList);
-        linksList.appendChild(weekItem);
+    lessons.forEach(lesson => {
+        const lessonItem = document.createElement("li");
+        const lessonTitle = document.createElement("h3");
+        lessonTitle.textContent = `${lesson.lesson}`;
+        lessonItem.appendChild(lessonTitle);
+
+        // const links = lesson.links;
+        // const linksList = document.createElement("ul");
+        // links.forEach(link => {
+        //     const linkItem = document.createElement("li");
+        //     const linkAnchor = document.createElement("a");
+        //     linkAnchorlinkItem.textContent = link.title;
+        //     linkAnchor.setAttribute("href", link.url);
+        //     linkItem.appendChild(linkAnchor);
+        //     linksList.appendChild();
+        // });
+        linksList.appendChild(lessonItem);
     });
 }
   
