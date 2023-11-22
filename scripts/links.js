85e54588637ba7d90cfa17bc793edfa331cab235
Josh <sooaemalelagij@gmail.com>
2023-11-22 14:29:51 -0700

    Dynamic links from Json

--- scripts/links.js
+++ scripts/links.js
@@ -12,26 +12,41 @@ async function getLinks() {
   
 function displayLinks(lessons) {
     const linksList = document.getElementById("linksList");
-    lessons.forEach(lesson => {
-        const lessonItem = document.createElement("li");
-        const lessonTitle = document.createElement("h3");
-        lessonTitle.textContent = `${lesson.lesson}`;
-        lessonItem.appendChild(lessonTitle);
 
-        // const links = lesson.links;
-        // const linksList = document.createElement("ul");
-        // links.forEach(link => {
-        //     const linkItem = document.createElement("li");
-        //     const linkAnchor = document.createElement("a");
-        //     linkAnchorlinkItem.textContent = link.title;
-        //     linkAnchor.setAttribute("href", link.url);
-        //     linkItem.appendChild(linkAnchor);
-        //     linksList.appendChild();
-        // });
-        linksList.appendChild(lessonItem);
+    lessons.forEach(lesson => {
+        // Create a list item for each lesson
+        const listItem = document.createElement("li");
+
+        // Create and set the lesson title
+        const lessonTitle = document.createElement("h3");
+        lessonTitle.textContent = lesson.lesson;
+        listItem.appendChild(lessonTitle);
+
+        // Create a div to hold the links
+        const linksDiv = document.createElement("div");
+        linksDiv.className = "lesson-links";
+
+        // Iterate over the links in each lesson
+        lesson.links.forEach(link => {
+            const lessonLink = document.createElement("a");
+            lessonLink.setAttribute("href", link.url);
+            lessonLink.textContent = link.title;
+
+            // Append the link to the links div
+            linksDiv.appendChild(lessonLink);
+
+            // Optional: Add a line break for better readability
+            linksDiv.appendChild(document.createElement("br"));
+        });
+
+        // Append the links div to the list item
+        listItem.appendChild(linksDiv);
+
+        // Append the list item to the main list
+        linksList.appendChild(listItem);
     });
 }
-  
+
   getLinks();
 
 
